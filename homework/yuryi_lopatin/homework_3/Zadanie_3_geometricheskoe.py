1b34c857c250b882e41d0231b112a5f80dbe4abf
YuryiLo <uryi.84@mail.ru>
2024-12-20 19:02:34 +0300

    files from homeworks_3

---
+++ homework/yuryi_lopatin/homework_3/Zadanie_3_geometricheskoe.py
@@ -0,0 +1,5 @@
+from math import sqrt
+
+c = int(input(3))
+d = int(input(10))
+print(type(int(sqrt(c + d))))
---
+++ homework/yuryi_lopatin/homework_3/zadanie_4.py
@@ -0,0 +1,6 @@
+import math
+
+ab = 8
+bc = 15
+ac2 = ab ** 2 + bc ** 2
+print(math.sqrt(ac2))
