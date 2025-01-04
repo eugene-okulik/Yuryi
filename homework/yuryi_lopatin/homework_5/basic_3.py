59d05c2dd7ceb4c37623239e2f2b9531f901ba96
YuryiLo <uryi.84@mail.ru>
2025-01-04 09:39:09 +0300

    homework_5 review 2 zadaniya

--- homework/yuryi_lopatin/homework_5/basic_3.py
+++ homework/yuryi_lopatin/homework_5/basic_3.py
@@ -1,17 +1,15 @@
-from ctypes import HRESULT
-
 person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
-a, b, c, d, e = person
-print(a, b, c, d, e)
+name, last_name, city, phone_number, state = person
+print(name, last_name, city, phone_number, state)
 
 
 str1 = 'результат операции: 42'
 str2 = 'результат операции: 514'
 str3 = 'результат работы программы: 9'
 
-num1 = str1.split(':')[1]
-num2 = str2.split(':')[1]
-num3 = str3.split(':')[1]
+num1 = (str1.index('42'))
+num2 = (str2.index('514'))
+num3 = (str3.index('9'))
 
 print(int(num1) + 10)
 print(int(num2) + 10)
