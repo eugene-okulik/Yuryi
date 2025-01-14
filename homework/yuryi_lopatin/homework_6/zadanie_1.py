ee16eebd1829dd22f0b15e650e0335a45067cb9d
YuryiLo <uryi.84@mail.ru>
2025-01-14 06:17:17 +0300

    homework_6

---
+++ homework/yuryi_lopatin/homework_6/zadanie_1.py
@@ -0,0 +1,9 @@
+from pygments.lexer import words
+
+text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, '
+        'dignissim vitae libero')
+words = text.split()
+for word in words:
+    word = word.replace(',', '')
+    word = word.replace('.', '')
+    print((word) + 'ing')
