7028388867e79eb6b50b1cc88521d263bb6d0aa5
YuryiLo <uryi.84@mail.ru>
2025-01-16 11:01:32 +0300

    homework_6 zadanie_1 review

--- homework/yuryi_lopatin/homework_6/zadanie_1.py
+++ homework/yuryi_lopatin/homework_6/zadanie_1.py
@@ -1,7 +1,15 @@
-text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, '
-        'dignissim vitae libero')
+text: str = (
+    'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, '
+    'dignissim vitae libero.')
 words = text.split()
+final_text = []
+
 for word in words:
-    word = word.replace(',', '')
-    word = word.replace('.', '')
-    print((word) + 'ing')
+    if word[-1] in ',.':
+        punctution = word[-1]
+        main_word = word[:-1] + 'ing' + punctution
+    else:
+        main_word = word + 'ing'
+    final_text.append(main_word)
+print(' '.join(final_text))
+
