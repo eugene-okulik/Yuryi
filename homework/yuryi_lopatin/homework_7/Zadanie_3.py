6dd44da2a43375065f721d062a8d21cb97797c01
YuryiLo <uryi.84@mail.ru>
2025-01-20 20:08:23 +0300

    homework_7_vse_zadaniya

--- homework/yuryi_lopatin/homework_7/Zadanie_3.py
+++ homework/yuryi_lopatin/homework_7/Zadanie_3.py
@@ -6,9 +6,9 @@ list_from_str1 = str1.split(' ')
 list_from_str2 = str2.split(' ')
 list_from_str3 = str3.split(' ')
 
-numbers1 = list_from_str1[-1] # <class 'str'> 42
-numbers2 = list_from_str2[-1] # <class 'str'> 514
-numbers3 = list_from_str3[-1] # <class 'str'> 9
+numbers1 = list_from_str1[-1]
+numbers2 = list_from_str2[-1]
+numbers3 = list_from_str3[-1]
 
 print(int(numbers1) + 10)
 print(int(numbers2) + 10)
--- homework/yuryi_lopatin/homework_7/zadanie_2.py
+++ homework/yuryi_lopatin/homework_7/zadanie_2.py
@@ -1,5 +1,5 @@
-words = {'I': 3, 'love': 5, 'Python': 1, '!': 50} # Словарь
-def words_num(word, num): # Функция, принимающая два параметра word и num
+words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
+def words_num(word, num):
     print(f'{word*num}')
 words_num('I', 3)
 words_num('love', 5)
