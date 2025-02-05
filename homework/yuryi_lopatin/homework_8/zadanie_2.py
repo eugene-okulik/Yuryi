207d50da973868ebe1b20038c42c855bf115f975
YuryiLo <uryi.84@mail.ru>
2025-02-05 19:01:37 +0300

    Homework_8

--- homework/yuryi_lopatin/homework_8/zadanie_2.py
+++ homework/yuryi_lopatin/homework_8/zadanie_2.py
@@ -1,6 +1,7 @@
 import sys
 sys.set_int_max_str_digits(0)
 
+
 def fibonacci(n):
     fib1, fib2 = 0, 1
     count = 0
@@ -9,6 +10,7 @@ def fibonacci(n):
         count += 1
         if count in [5, 200, 1000, 100000]:
             yield fib1
+
 
 for fib in fibonacci(100001):
     print(fib, end=' ')
