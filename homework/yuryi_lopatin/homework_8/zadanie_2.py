a506097ff552ed2fe0da19b59296bb86d3a499bf
YuryiLo <uryi.84@mail.ru>
2025-02-06 18:01:04 +0300

    Homework_8_rewiev_fiban

--- homework/yuryi_lopatin/homework_8/zadanie_2.py
+++ homework/yuryi_lopatin/homework_8/zadanie_2.py
@@ -1,8 +1,9 @@
 import sys
 sys.set_int_max_str_digits(0)
 
+
 rng = [5, 200, 1000, 100000]
-#Этот генератор должен только выдавать последовательность чисел фибоначчи
+
 def fibonacci(n, rng):
     fib1, fib2 = 0, 1
     count = 0
@@ -12,8 +13,7 @@ def fibonacci(n, rng):
         if count in rng:
             yield fib1
 
-#А здесь, пользуясь генератором нужно получать числа и распечатывать только те, которые нужны.
+
 for fib in fibonacci(100001, rng):
     print(fib, end=' ')
 print()
-
