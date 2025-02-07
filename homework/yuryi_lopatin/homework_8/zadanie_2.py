20b594f94f4bfd2f9a3f6deff2a30242c7065155
YuryiLo <uryi.84@mail.ru>
2025-02-07 19:55:00 +0300

    Homework_8_rewiev2_fibanachi

--- homework/yuryi_lopatin/homework_8/zadanie_2.py
+++ homework/yuryi_lopatin/homework_8/zadanie_2.py
@@ -5,16 +5,14 @@ sys.set_int_max_str_digits(0)
 rng = [5, 200, 1000, 100000]
 
 
-def fibonacci(n, rng):
-    fib1, fib2 = 0, 1
-    count = 0
-    for i in range(n):
-        fib1, fib2 = fib2, fib1 + fib2
-        count += 1
-        if count in rng:
-            yield fib1
+def fibonacci(n):
+   fib1, fib2 = 0, 1
+   for i in range(n):
+       fib1, fib2 = fib2, fib1 + fib2
+       yield fib1
 
-
-for fib in fibonacci(100001, rng):
-    print(fib, end=' ')
-print()
+count = 0
+for fib in fibonacci(100001):
+   count += 1
+   if count in rng:
+       print(fib)
