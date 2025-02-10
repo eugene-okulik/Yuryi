7e762b1576f76c56b2a9fa33cf9017cf6245dc21
YuryiLo <uryi.84@mail.ru>
2025-02-10 19:34:58 +0300

    homework_9

--- homework/yuryi_lopatin/homework_9/zadanie_2.py
+++ homework/yuryi_lopatin/homework_9/zadanie_2.py
@@ -1,6 +1,6 @@
 temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32,
                 30, 28, 24, 23]
-temperatures = set (temperatures)
+temperatures = set(temperatures)
 temperatures = list(temperatures)
 
 
@@ -9,6 +9,7 @@ def many_28(x):
         return True
     else:
         return False
+
 
 hot_temperaturs = filter(many_28, temperatures)
 hot_list = list(hot_temperaturs)
