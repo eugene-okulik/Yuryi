b15b4d5332616652101d09f48c2543416234fe3f
YuryiLo <uryi.84@mail.ru>
2025-02-24 16:31:16 +0300

    Work10

--- homework/yuryi_lopatin/homework_10/dz_3_arefm.py
+++ homework/yuryi_lopatin/homework_10/dz_3_arefm.py
@@ -5,7 +5,6 @@ second = int(input('Enter second number: '))
 def run_calc(func):
 
     def wrapper(first, second, operation):
-        result = func(first, second, operation)
         if first < 0 or second < 0:
             result = first * second
         elif first == second:
@@ -14,14 +13,14 @@ def run_calc(func):
             result = second - first
         elif second > first:
             result = first / second
-        else:
-            'Введены неверные значения'
+
         return result
 
     return wrapper
 
 
 @run_calc
+
 def calc(first, second, operation):
     if operation == '*':
         return first * second
@@ -30,8 +29,6 @@ def calc(first, second, operation):
     elif operation == '-':
         return second - first
     elif operation == '/':
-        if second == 0:
-            return "Ошибка: деление на ноль"
         return first / second
     else:
         return 'Введены неверные значения'
