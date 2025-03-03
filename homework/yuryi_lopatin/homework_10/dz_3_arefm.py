c59b7ead9a03d2b84256f22b47173456604dbbec
YuryiLo <uryi.84@mail.ru>
2025-03-03 12:12:09 +0300

    Work11

--- homework/yuryi_lopatin/homework_10/dz_3_arefm.py
+++ homework/yuryi_lopatin/homework_10/dz_3_arefm.py
@@ -33,5 +33,4 @@ def calc(first, second, operation):
     else:
         return 'Введены неверные значения'
 
-
 print(calc(first, second, 'введите число'))
---
+++ homework/yuryi_lopatin/homework_11/create_classes_book.py
@@ -0,0 +1,59 @@
+class Book():
+    have_text = True
+
+    def __init__(self, title, author, pages, material, book_type, reserve):
+        self.title = title
+        self.author = author
+        self.pages = pages
+        self.material = material
+        self.book_type = book_type
+        self.reserve = reserve
+
+    def isbn(self):
+        return self.isbn_number
+
+    def print(self):
+        if self.reserve:
+            return (f'Название: {self.title}, Автор: {self.author}, страниц {self.pages}:, '
+                    f'материал: {self.material}, зарезервирована')
+        else:
+            return (f'Название: {self.title}, Автор: {self.author}, страниц: {self.pages}, '
+                    f'материал: {self.material}')
+
+class SchoolBook(Book):
+    def __init__(self, title, author, pages, material, book_type, reserve, subject, school_class, homework):
+        super().__init__(title, author, pages, material, book_type, reserve)
+        self.subject = subject
+        self.school_class = school_class
+        self.homework = homework
+
+    def print(self):
+        if self.homework:
+            return (f'Название: {self.title}, Автор: {self.author}, предмет: {self.subject}, зарезервирована')
+        else:
+            return (f'Название: {self.title}, Автор: {self.author}, предмет: {self.subject}')
+
+matematika_5_class = SchoolBook('Matematika 5 class', 'Gipakra', 211, 'paper',
+                                'school book', True, 'Matematika', '5 class', True)
+geogerphy = SchoolBook('Geopraphy 7 class', 'Christophor Columb', 240, 'paper',
+                       'school book', False, 'Matematika', '7 class', False)
+history = SchoolBook('History 3 class', 'Isac Son', 210, 'paper',
+                     'school book',False, 'Matematika', '9 class', False)
+
+club_5_am = Book('Club_5_am', 'Robin Sharma', 511, 'paper', 'psyhology', True)
+idiot = Book('Idiot', 'Dostoyevski', 640, 'paper', 'fiction', False)
+ispoved_huligana = Book('Ispoved Huligana', 'Esenin', 320, 'paper','poetry',
+                       False)
+steve_jobs = Book('Steve Jobs', 'Issaacson', 610, 'paper', 'biography',
+                  False)
+cats_hause = Book('Steve Jobs', 'Issaacson', 610, 'paper', 'biography',
+                  False)
+
+print(matematika_5_class.__dict__)
+print(geogerphy.__dict__)
+print(history.__dict__)
+print(club_5_am.__dict__)
+print(idiot.__dict__)
+print(ispoved_huligana.__dict__)
+print(steve_jobs.__dict__)
+print(cats_hause.__dict__)
