c7b4c28a6abb8769c098ec8873f0e782d33d15ef
YuryiLo <uryi.84@mail.ru>
2025-03-05 18:52:42 +0300

    homework11

--- homework/yuryi_lopatin/homework_11/create_classes_book.py
+++ homework/yuryi_lopatin/homework_11/create_classes_book.py
@@ -1,24 +1,26 @@
 class Book():
     have_text = True
 
-    def __init__(self, title, author, pages, material, reserve):
+    def __init__(self, title, author, pages, material, isbn, reserve):
         self.title = title
         self.author = author
         self.pages = pages
         self.material = material
+        self.isbn = isbn
         self.reserve = reserve
 
     def print(self):
         if self.reserve:
             return (f'Название: {self.title}, Автор: {self.author}, страниц: {self.pages}:, материал: {self.material}, '
-                    f'зарезервирована')
+                    f'isbn: {self.isbn}, зарезервирована')
         else:
-            return (f'Название: {self.title}, Автор: {self.author}, страниц: {self.pages}, материал: {self.material}')
+            return (f'Название: {self.title}, Автор: {self.author}, страниц: {self.pages}, материал: {self.material}, '
+                    f'isbn: {self.isbn}')
 
 
 class SchoolBook(Book):
-    def __init__(self, title, author, pages, book_type, reserve, subject, school_class, homework):
-        super().__init__(title, author, pages, book_type, reserve)
+    def __init__(self, title, author, pages, book_type, isbn, reserve, subject, school_class, homework):
+        super().__init__(title, author, pages, book_type, isbn, reserve)
         self.subject = subject
         self.school_class = school_class
         self.homework = homework
@@ -32,15 +34,18 @@ class SchoolBook(Book):
                     f'класс: {self.school_class}')
 
 
-matematika_5_class = SchoolBook('Математика', 'Евклид', 211, 'school book', True, 'математика', 5, True)
-geography = SchoolBook('География', 'Колумб', 240, 'school book', False, 'география', 7, False)
-history = SchoolBook('История', 'Герадот', 210, 'school book', False, 'история', 9, False)
+matematika_5_class = SchoolBook('Математика', 'Евклид', 211, 'school book', 2128511,
+                                True, 'математика', 5, True)
+geography = SchoolBook('География', 'Колумб', 240, 'school book', 2128512,
+                       False, 'география', 7, False)
+history = SchoolBook('История', 'Герадот', 210, 'school book', 2128513, False,
+                     'история', 9, False)
 
-club_5_am = Book('Клуб 5 утра', 'Шарма', 511, 'бумага', True)
-idiot = Book('Идиот', 'Достоевский', 640, 'бумага', False)
-ispoved_huligana = Book('Исповедь хулигана', 'Есенин', 320, 'бумага', False)
-steve_jobs = Book('Стив Джобс', 'Исаксон', 610, 'бумага', False)
-cats_hause = Book('Кошкин дом', 'Маршак', 42, 'бумага', False)
+club_5_am = Book('Клуб 5 утра', 'Шарма', 511, 'бумага', 2128506, True)
+idiot = Book('Идиот', 'Достоевский', 640, 'бумага', 2128507, False)
+ispoved_huligana = Book('Исповедь хулигана', 'Есенин', 320, 'бумага', 2128508, False)
+steve_jobs = Book('Стив Джобс', 'Исаксон', 610, 'бумага', 2128509, False)
+cats_hause = Book('Кошкин дом', 'Маршак', 42, 'бумага', 2128510, False)
 
 print(matematika_5_class.print())
 print(geography.print())
