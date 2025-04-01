52d3c0e066bf3890d7ef88c48b2238260495bbd8
YuryiLo <uryi.84@mail.ru>
2025-04-01 16:08:32 +0300

    Home+work13

--- homework/yuryi_lopatin/Homework_13/Homework_13.py
+++ homework/yuryi_lopatin/Homework_13/Homework_13.py
@@ -4,8 +4,11 @@ from datetime import datetime
 from os.path import split
 
 
-file_path = '../../eugene_okulik/hw_13/data.txt'
-with open(file_path, 'r', encoding='utf8') as file:
+it_place = os.path.dirname(__file__)
+homew_path = os.path.dirname(os.path.dirname(it_place))
+eugene_ok_path = os.path.join(homew_path, 'eugene_okulik', 'hw_13', 'data.txt')
+
+with open(eugene_ok_path, 'r', encoding='utf8') as file:
     lines = file.readlines()
 
 date_string1 = lines[0].split(' - ')[0].split('. ')[1]
@@ -14,9 +17,7 @@ print(date_obj1 + timedelta(days=7))
 
 date_string2 = lines[1].split(' - ')[0].split('. ')[1]
 date_obj2 = datetime.strptime(date_string2, '%Y-%m-%d %H:%M:%S.%f')
-day_weekdays = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье']
-day_weekdays = day_weekdays[date_obj2.weekday()]
-print(f'{date_string2} - это {day_weekdays}')
+print(date_obj2.strftime("%A"))
 
 date_string3 = lines[2].split(' - ')[0].split('. ')[1]
 date_obj3 = datetime.strptime(date_string1, '%Y-%m-%d %H:%M:%S.%f')
