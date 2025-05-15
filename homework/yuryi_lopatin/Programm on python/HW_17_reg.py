ae1153e097734352620e8b6384e992152b53d8b2
YuryiLo <uryi.84@mail.ru>
2025-05-15 13:58:57 +0300

    Homework_programm_on_python

--- homework/yuryi_lopatin/Programm on python/HW_17_reg.py
+++ homework/yuryi_lopatin/Programm on python/HW_17_reg.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
-'''
-Скрипт для поиска текста в файлах
-'''
-# ИМПОРТЫ:
-# Основные библиотеки для работы с файловой системой и регулярными выражениями
 import os       # для работы с путями и файловой системой
 import re       # для регулярных выражений (пока не используется)
 import sys      # для доступа к аргументам командной строки
 import argparse # для удобного разбора аргументов командной строки (пока не используется)
 from calendar import error
 
+
 base_path = os.path.dirname(__file__)   # запишет текущий рабочий каталога в переменную base_path
 homew_path = os.path.dirname(os.path.dirname(base_path)) # Переход на папку выше
 file_path = os.path.join(homew_path, 'eugene_okulik', 'data', 'logs') # Формирование пути к каталогу с логами
+
 
 # ПОИСК ФАЙЛОВ:
 result = []  # Создаем пустой список, который будет хранить результаты поиска. В список будут добавляться индексы
@@ -21,6 +18,7 @@ def get_files(path, search_text=None):
     for file in os.listdir(path):
         if os.path.isfile(os.path.join(path, file)):
             yield file
+
 for file in get_files(file_path, 'error'):
     search_text = "ERROR"
     with open(os.path.join(file_path, file), 'r', encoding='utf-8') as f:
