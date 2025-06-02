cff066b543997847d10d00c4009a62a994e6df52
YuryiLo <uryi.84@mail.ru>
2025-06-02 15:13:13 +0300

    home_work_18_test_api

--- homework/yuryi_lopatin/Homework_18_test_API/test_api.py
+++ homework/yuryi_lopatin/Homework_18_test_API/test_api.py
@@ -5,6 +5,8 @@ import json
 def get_all_obj():
     response = requests.get('http://167.172.172.115:52353/object').json()
     print(response)
+
+
 get_all_obj()
 
 
@@ -14,6 +16,8 @@ def new_object():
     response = requests.post('http://167.172.172.115:52353/object', json=body, headers=headers).json()
     print(response)
     return response['id']
+
+
 new_object()
 
 
@@ -21,6 +25,9 @@ def get_new_object():
     post_id = new_object()
     response = requests.get(f'http://167.172.172.115:52353/object/{post_id}').json()
     print(response)
+    return response
+
+
 get_new_object()
 
 
@@ -34,10 +41,13 @@ def put_object():
     assert response['name'] == 'saks'
     assert response['data'] == {"color": "red", "size": "small"}
     print(response)
+    return response
+
+
 put_object()
 
 
-def patch_post():
+def patch_object():
     post_id = new_object()  # 409 #new_post() #1
     body = {
         "name": "ass",
@@ -49,13 +59,18 @@ def patch_post():
     assert response['name'] == 'ass'
     assert response['data'] == {"color": "pink", "size": "very big"}
     print(response)
-patch_post()
+    return response
 
 
-def delete_a_post():
+patch_object()
+
+
+def delete_a_object():
     post_id = new_object()
     response = requests.delete(f'http://167.172.172.115:52353/object/{post_id}')
     assert response.status_code == 200, 'status code'
     print(response)
     print(response.status_code)
-delete_a_post()
+
+
+delete_a_object()
