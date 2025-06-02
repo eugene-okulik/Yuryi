ec59def3f6cb3edf3a29e2411d30221260ef588b
YuryiLo <uryi.84@mail.ru>
2025-06-02 14:13:38 +0300

    test_api

---
+++ homework/yuryi_lopatin/Homework_18_test_API/test_api.py
@@ -0,0 +1,63 @@
+import requests
+import json
+
+
+#GET all obj
+def get_all_obj():
+    response = requests.get('http://167.172.172.115:52353/object').json()
+    print(response)
+get_all_obj()
+
+#POST
+def new_object():
+    body = {"name": "lox", "data": {"color": "white", "size": "big"}}
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post('http://167.172.172.115:52353/object', json = body, headers = headers).json()
+    print(response)
+    return response['id']
+new_object()
+
+#GET new obj
+def get_new_object():
+    post_id = new_object()
+    response = requests.get(f'http://167.172.172.115:52353/object/{post_id}').json()
+    #assert response['id'] == post_id # без assert отдаст [], с assert ошибка т.к. в [] нет 'id'
+    print(response)
+get_new_object()
+
+#PUT with assert
+def put_object():
+    post_id = new_object()
+    body = {
+        "name": "saks",
+        "data": {"color": "red", "size": "small"}
+    }
+    response = requests.put(f'http://167.172.172.115:52353/object/{post_id}', json = body).json()
+    assert response['name'] == 'saks'
+    assert response['data'] == {"color": "red", "size": "small"}
+    print(response)
+put_object()
+
+#PATCH with assert
+def patch_post():
+    post_id = new_object()  # 409 #new_post() #1
+    body = {
+        "name": "ass",
+        "data": {"color": "pink", "size": "very big"}
+    }
+    headers = {'Content-Type': 'application/json'}
+    response = (requests.patch(f'http://167.172.172.115:52353/object/{post_id}', json = body, headers = headers)
+                .json())
+    assert response['name'] == 'ass'
+    assert response['data'] == {"color": "pink", "size": "very big"}
+    print(response)
+patch_post()
+
+#DELETE with assert
+def delete_a_post():
+    post_id = new_object()
+    response = requests.delete(f'http://167.172.172.115:52353/object/{post_id}')
+    assert response.status_code == 200, 'status code'
+    print(response)
+    print(response.status_code)
+delete_a_post()
