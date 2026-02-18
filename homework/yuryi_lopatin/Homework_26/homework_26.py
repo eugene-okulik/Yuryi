233b3a74682fb00c446f9d66cf534b13228c51a4
YuryiLo <uryi.84@mail.ru>
2026-02-18 08:37:44 +0300

    playwrite tests

--- homework/yuryi_lopatin/Homework_26/homework_26.py
+++ homework/yuryi_lopatin/Homework_26/homework_26.py
@@ -13,8 +13,7 @@ def driver():
     chrome_driver = webdriver.Chrome()
     chrome_driver.implicitly_wait(6)  # Не явное ожидание. Используется редко, может вызывать конфликт с явными ожид.
     chrome_driver.maximize_window()
-    yield chrome_driver
-    sleep(3)
+    return chrome_driver
 
 
 def test_shop_with_action_chains(driver):
@@ -46,21 +45,21 @@ def test_shop_with_action_chains(driver):
 def test_in_shop2(driver):
     wait = WebDriverWait(driver, 10)
     driver.get('http://testshop.qa-practice.com/')
-    print("1. Ищем товар...")
+    # print("1. Ищем товар...")
     photo = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[href="/shop/customizable-desk-9"]')))
-    print("2. Наводим курсор на товар...")
+    # print("2. Наводим курсор на товар...")
     actions = ActionChains(driver)
     actions.move_to_element(photo).perform()
-    print("3. Ищем иконку корзины...")
+    # print("3. Ищем иконку корзины...")
     basket = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'o_wsale_product_btn')))
-    print("4. Делаем простой клик...", basket)
+    # print("4. Делаем простой клик...", basket)
     basket.click()
-    print("✓ Клик выполнен")
+    # print("✓ Клик выполнен")
     popup = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.modal.show')))
-    print("✓ Попап с конфигуратором появился")
+    # print("✓ Попап с конфигуратором появился")
     modal_body = popup.find_element(By.CLASS_NAME, 'modal-body')
     modal_text = modal_body.text.lower()
-    print(f"Текст в модале:\n{modal_text}")  # Получаем весь текст из модального окна
+    # print(f"Текст в модале:\n{modal_text}")  # Получаем весь текст из модального окна
     # Проверяем наличие ключевых слов товара
     assert "customizable" in modal_text and "desk" in modal_text, "Товар 'Customizable Desk' не найден в попапе!"
     assert "legs" in modal_text, "Секция LEGS не найдена в попапе конфигуратора"  # Проверяем, что товар совпадает
