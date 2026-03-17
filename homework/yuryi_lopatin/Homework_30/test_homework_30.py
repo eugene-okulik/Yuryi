c8d19c0c1c90f9c3f866ad6784926748878800c1
YuryiLo <uryi.84@mail.ru>
2026-03-17 14:01:09 +0300

    Homework_30

--- homework/yuryi_lopatin/Homework_30/test_homework_30.py
+++ homework/yuryi_lopatin/Homework_30/test_homework_30.py
@@ -11,7 +11,7 @@ def test_modify_specific_api(page: Page):
     def change_req(route: Route):
         response = route.fetch()
         print(response)
-        print(f"\n🎯 Перехвачен API запрос:")
+        print(f"\n Перехвачен API запрос:")
         print(f"{route.request.url}")
         try:
             data = response.json()
@@ -20,7 +20,7 @@ def test_modify_specific_api(page: Page):
                 response=response,
                 json=modified
             )
-        except:
+        except Exception:
             route.fulfill(response=response)
 
     page.route(re.compile(r'.*(api|\.json).*'), change_req)
@@ -30,5 +30,5 @@ def test_modify_specific_api(page: Page):
     page.locator('.rf-hcard-copy').first.click()
     page.wait_for_timeout(3000)
     header = page.locator('.rf-digitalmat-overlay-header').first.inner_text()
-    print(f"\n📦 Заголовок: {header}")
+    print(f"\n Заголовок: {header}")
     print("=" * 70 + "\n")
