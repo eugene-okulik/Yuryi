4bba2b4a8af1f60b2345247f6869e98afded4c1e
YuryiLo <uryi.84@mail.ru>
2026-03-17 13:46:54 +0300

    home_work_30

--- homework/yuryi_lopatin/Homework_30/test_homework_30.py
+++ homework/yuryi_lopatin/Homework_30/test_homework_30.py
@@ -7,11 +7,12 @@ def test_modify_specific_api(page: Page):
     print("\n" + "=" * 70)
     print("ТЕСТ ПЕРЕХВАТА ЗАПРОСОВ")
     print("=" * 70)
+
     def change_req(route: Route):
         response = route.fetch()
         print(response)
         print(f"\n🎯 Перехвачен API запрос:")
-        print(f"   {route.request.url}")
+        print(f"{route.request.url}")
         try:
             data = response.json()
             modified = json.loads(json.dumps(data).replace('iPhone', 'Яблокофон'))
@@ -28,7 +29,6 @@ def test_modify_specific_api(page: Page):
 
     page.locator('.rf-hcard-copy').first.click()
     page.wait_for_timeout(3000)
-    
     header = page.locator('.rf-digitalmat-overlay-header').first.inner_text()
     print(f"\n📦 Заголовок: {header}")
     print("=" * 70 + "\n")
